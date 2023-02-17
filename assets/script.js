5084eed3c4156dda2f1375252bc9dd0a0089cf01
bckmnn <stfn@bckmnn.com>
2023-02-17 22:02:36 +0100

    updates

--- assets/script.js
+++ assets/script.js
@@ -4,14 +4,39 @@ var retry_delay = 1000;
 var interval = setInterval(connect, retry_delay);
 let myFont
 
+let font
+let msg
+let fSize
+
+let txtPoints, txtBounds
+
+let cam;
+
 function preload() {
     myFont = loadFont('assets/fonts/D-DIN.otf');
+    opentype.load('assets/fonts/D-DIN.otf', function (err, f) {
+        if (err) {
+            alert('Font could not be loaded: ' + err);
+        } else {
+            font = f
+            console.log('font ready')
+
+            fSize = 256
+            msg = 'dDisplay'
+
+            path = font.getPath(msg, 0, 0, fSize)
+            console.log(path.commands)
+        }
+    })
 }
-  
+
 
 function setup() {
     createCanvas(windowWidth, windowHeight, WEBGL);
+    cam = new Dw.EasyCam(this._renderer, {distance : 900});
+    document.oncontextmenu = ()=>false;
     smooth();
+    setAttributes('antialias', true);
     textFont(myFont);
     textSize(32);
     textAlign(CENTER, CENTER);
@@ -20,32 +45,76 @@ function setup() {
 
 function draw_connection_state() {
     push();
-    if(ws == undefined || ws.readyState == ws.CLOSED){
-        fill(255,0,0);
-    }else if (ws.readyState == ws.OPEN){
-        fill(0,255,0);
-    }else{
-        fill(255,165,0);
+    if (ws == undefined || ws.readyState == ws.CLOSED) {
+        fill(255, 0, 0);
+    } else if (ws.readyState == ws.OPEN) {
+        fill(0, 255, 0);
+    } else {
+        fill(255, 165, 0);
     }
-    ellipse(10,10,10,10);
+    ellipse(10, 10, 10, 10);
+    pop();
+}
+
+function drawText3D(z) {
+    if (txtBounds == null) {
+        txtBounds = myFont.textBounds(msg, 0, 0, 256);
+    }
+    push();
+    
+    let locX = mouseX - height / 2;
+    let locY = mouseY - width / 2;
+    
+    translate(-txtBounds.w /2, -txtBounds.h /2,z);
+    noFill()
+    
+    stroke(255)
+
+    for (let cmd of path.commands) {
+        if (cmd.type === 'M') {
+            beginShape()
+            vertex(cmd.x, cmd.y)
+        } else if (cmd.type === 'L') {
+            vertex(cmd.x, cmd.y)
+        } else if (cmd.type === 'C') {
+           bezierVertex(cmd.x1, cmd.y1, cmd.x2, cmd.y2, cmd.x, cmd.y)
+        } else if (cmd.type === 'Q') {
+            quadraticVertex(cmd.x1, cmd.y1, cmd.x, cmd.y)
+        } else if (cmd.type === 'Z') {
+            endShape(CLOSE)
+        }
+    }
+
     pop();
 }
 
 
 
 function draw() {
+    background(0,0.3)
 
+    for (let z = -10; z < 20; z+=5) {
+        drawText3D(z)
+    }
+
+    
     push();
-
+    
+    stroke(125);
+    fill(125);
+    strokeWeight(1);
 
     text(ddisplay_id, 0, 0);
-
     translate(-width / 2, -height / 2);
-    stroke(125);
-    strokeWeight(1);
-    fill(125);
 
+    
+
+    
+    pop();
+
+    cam.beginHUD();
     draw_connection_state();
+    cam.endHUD();
 }
 
 function connect() {
@@ -67,7 +136,7 @@ function connect() {
         };
 
         ws.onmessage = (msg) => {
-            var cmds = {"iam": iam, "set": set, "bytes": byt };
+            var cmds = { "iam": iam, "set": set, "bytes": byt };
             if (msg.data) {
                 var parts = msg.data.split(" ")
                 var cmd = cmds[parts[0]];
